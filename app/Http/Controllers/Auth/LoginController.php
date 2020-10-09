14483a778f919653b776fcf079dd269175e815c2
Mohamadelattar <mohamadelattar99@gmail.com>
2020-10-09 10:50:51 +0100

    Finish the project

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,35 +5,50 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = '/home';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectToProvider($service)
+    {
+        return Socialite::driver($service)->redirect();
+    }
+
+    public function handleProviderCallback($service)
+    {   
+        if($service == "google") {
+            $user = Socialite::driver($service)->stateless()->user();
+        }else {
+            $user = Socialite::driver($service)->user();
+        }
+
+        $findUser = User::where('email', $user->getEmail())->first();
+        if($findUser) {
+            Auth::login($findUser);
+        }else {
+
+            $newUser = new User;
+            $newUser->name = $user->getName();
+            $newUser->email = $user->getEmail();
+            $newUser->password = bcrypt(12345678);
+            
+            if($newUser->save()) {
+                Auth::login($newUser);
+                return redirect('/');
+            } else {
+                return abort(404);
+            }
+        }
+        return redirect('/');
     }
 }
